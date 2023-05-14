683edcb096d29dfd2df15085522b9483618a7a06
Nikolaos <lunaticnick@hotmail.com>
2023-05-14 20:04:06 +0100

    use brew bundle

---
+++ Brewfile
@@ -0,0 +1,20 @@
+#  Taps
+tap "homebrew/bundle"
+tap "homebrew/core"
+
+# Brews (Packages)
+brew "bat"
+brew "blueutil"
+brew "fzf"
+brew "httpie"
+brew "zoxide"
+brew "zsh"
+brew "zsh-syntax-highlighting"
+brew "exa"
+
+# Casks (Applications)
+cask "alfred"
+cask "visual-studio-code"
+cask "dropbox"
+
+
--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -4,12 +4,11 @@ echo "\n ===== Start Homebrew Setup ======\n"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install httpie
-brew install bat
+# brew install httpie
+# brew install bat
+# brew install --no-quarantine visual-studio-code
+# brew install --no-quarantine google-chrome
+# brew install --no-quarantine alfred
 
-brew install --no-quarantine visual-studio-code
-brew install --no-quarantine google-chrome
-brew install --no-quarantine alfred
-
-
+brew bundle --verbose
 
